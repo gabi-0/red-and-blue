61c659964775965ef30bd33bafd370a1a01f6c3d
gabi-0 <gmihalcea5@gmail.com>
2024-02-12 13:51:45 +0200

    created triangle

--- src/randb/Board.js
+++ src/randb/Board.js
@@ -1,4 +1,5 @@
 
+import "./board.css"
 
 function BoardCell({id}) {
 	var c_id = "cell_"+ id;
@@ -9,10 +10,12 @@ export default function Board() {
 	var boardRows = [0, 3, 6];
 	var boardCells = [0, 1, 2];
 	return (
-	<table>
+	<div><table>
 		{boardRows.map((row) => (
 		<tr>{boardCells.map((cell) => ( <BoardCell id={row+cell+1} /> ))}</tr>
 		))}
-	</table>
+	</table> <img className="piece_triang" src={process.env.PUBLIC_URL + "/img/triangle12.svg"} alt="RED" /> <br /> 
+	<img className="piece_triang" src={process.env.PUBLIC_URL + "/img/triangleLarge.svg"} alt="RED" /> <br />
+	<img className="piece_triang" src={process.env.PUBLIC_URL + "/img/triangle9.svg"} alt="RED" /> </div>
 	);
 }
